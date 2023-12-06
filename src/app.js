340f83c3f16d04a9343a0ff2e821a11af31390a8
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-12-06 10:34:15 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = require('./app/routes/routes');
+const globalErrorHandler = require('./app/middlewares/globalErrorHandler');
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('uploads'));
+
+app.use('/api/v1', router);
+
+//global error handler
+app.use(globalErrorHandler);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config/config');
+
+console.log('cdvefegr');
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url);
+    console.log('MongoDB connected');
+    app.listen(config.port, () => {
+      console.log(
+        `Example app listening on port http://localhost:${config.port}`,
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
