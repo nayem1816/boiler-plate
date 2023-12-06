fb416e75bcbde29651ae75f43a9fa7284531a6f5
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-12-06 12:39:11 +0600

    second

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 
-console.log('Hello');
+console.log('Hello 1');
 
 async function main() {
   try {
